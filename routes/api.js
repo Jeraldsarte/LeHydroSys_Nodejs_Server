13f3d1fdcd63c5ce8e8e218d95227824c0a9d6b1
jerald.sarte <jerald.sarte@bisu.edu.ph>
2025-05-07 20:27:07 +0800

    fix db error

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const { db } = require('../db'); 
 
 // Helper function to convert a date to the Philippines timezone
 function toPhilippinesTime(date) {
